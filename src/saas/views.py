0d534fbcd961d43d7f7c834b6d1dd4cd6f1199e1
Donald Folayan <donfolayan@gmail.com>
2025-01-25 10:11:21 +0100

    Checkpoint 1

---
+++ src/saas/views.py
@@ -0,0 +1,30 @@
+import pathlib
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from visits.models import PageVisits
+
+this_dir = pathlib.Path(__file__).resolve().parent
+
+def home_view(request, *args, **kwargs):
+    my_title = "SaaS"
+    
+    qs = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path=request.path)
+    total_qs = qs.count()
+    total_page_qs = page_qs.count()
+    try:
+        percentage_visits = round(((total_page_qs/total_qs) * 100), 2)
+    except ZeroDivisionError:
+        percentage_visits = 0
+
+    page_context = {
+        "page_title": my_title,
+        "total_visits": total_qs,
+        "page_visits": total_page_qs,
+        "percentage_page_visits": percentage_visits
+    }
+    PageVisits.objects.create(path=request.path)
+
+    return render(request, "home.html", page_context)
