776ed2f48513386d4d0edd3851243cef34f92ca9
Donald Folayan <donfolayan@gmail.com>
2025-02-04 07:09:12 +0100

    feat:

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -1,7 +1,17 @@
+import helpers
+
 from typing import Any
 from django.core.management.base import BaseCommand
+
+VENDOR_STATICFILES = {
+    "flowbite.min.css": "https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.css",
+    "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.js",
+}
 
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
-        print("hello-world")
+        self.stdout.write("Downloading vendor static files")
+
+        for name, url in VENDOR_STATICFILES.items():
+            self.stdout.write(name, url)
