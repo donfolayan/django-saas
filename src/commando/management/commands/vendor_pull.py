9d3513302ac980a91f98376548f518d93617b357
Donald Folayan <donfolayan@gmail.com>
2025-02-04 18:44:56 +0100

    feat: finished working on custom command to update vendor static files

--- src/commando/management/commands/vendor_pull.py
+++ src/commando/management/commands/vendor_pull.py
@@ -1,5 +1,6 @@
 import helpers
 
+from django.conf import settings
 from typing import Any
 from django.core.management.base import BaseCommand
 
@@ -8,10 +9,27 @@ VENDOR_STATICFILES = {
     "flowbite.min.js": "https://cdn.jsdelivr.net/npm/flowbite@3.1.1/dist/flowbite.min.js",
 }
 
+STATICFILES_VENDOR_DIR = getattr(settings, "STATICFILES_VENDOR_DIR")
 
 class Command(BaseCommand):
     def handle(self, *args: Any, **options: Any):
         self.stdout.write("Downloading vendor static files")
-
+        completed_url = []
+        
         for name, url in VENDOR_STATICFILES.items():
-            self.stdout.write(name, url)
+            out_path = STATICFILES_VENDOR_DIR / name
+            dl_success = helpers.download_to_local(url, out_path)
+            if dl_success:
+                completed_url.append(url)
+            else:
+                self.stdout.write(
+                    self.style.ERROR(f"Failed to download {url}")
+                )
+        if set(completed_url) == set(VENDOR_STATICFILES.values()):
+            self.stdout.write(
+                self.style.SUCCESS('Successfully updated all vendor static files')
+            )
+        else:
+            self.stdout.write(
+                self.style.ERROR("Some vendor static files were not updated successfully")
+            )
